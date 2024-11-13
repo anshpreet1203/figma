dfdd100c221b243fbab5092a8aaaf563cabea56e
anshpreet5911 <anshsiuu@gmail.com>
2024-11-14 00:22:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -90,9 +90,24 @@ const App = () => {
 
       {/* BRANDS */}
 
-      <div className='brands'>
-        <img src='/Brands.jpg' alt='logo' className='logos' ></img>
-      </div>
+    <div className="brands">
+  <div className="brands-wrapper">
+    <img src="/ad.png" alt="logo" className="logos" />
+    <img src="/apple.png" alt="logo" className="logos" />
+    <img src="/google.png" alt="logo" className="logos"/>
+    <img src="/samsung.png" alt="logo" className="logos" />
+    <img src="/nike.png" alt="logo" className="logos" />
+
+    <img src="/ad.png" alt="logo" className="logos " />
+    <img src="/apple.png" alt="logo" className="logos" />
+    <img src="/google.png" alt="logo" className="logos" />
+    <img src="/samsung.png" alt="logo" className="logos" />
+    <img src="/nike.png" alt="logo" className="logos" />
+    </div>
+    </div>
+
+
+     
 
       {/* PROJECTS */}
       <div className="project" ref={projectsRef}>Projects</div>
